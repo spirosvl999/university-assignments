19f38f7568a5f3c4a6f7218a4f622866a2ab57db
Vlachos Spiridon <79493906+spirosvl999@users.noreply.github.com>
2025-05-22 02:36:14 +0300

    Update g12.m

--- Signals_and_Systems/g12.m
+++ Signals_and_Systems/g12.m
@@ -1,5 +1,5 @@
-t=-0.1:.001:0.1; %The requested time signal interval and its step 0.001 sec)
-z=cos(100*pi*t)+ cos(200*pi*t)+sin(500*pi*t);%The Signal
+t=-0.1:.001:0.1;                                     %The requested time signal interval and its step 0.001 sec)
+z=cos(100*pi*t)+ cos(200*pi*t)+sin(500*pi*t);        %The Signal
 figure('Name','Signal Creation');
 plot(t,z);
 legend('x(t)');
