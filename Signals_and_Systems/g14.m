7b8870224e7d7eeda3ecefe90f9cf477c1b11544
Vlachos Spiridon <79493906+spirosvl999@users.noreply.github.com>
2025-05-22 02:37:47 +0300

    Update g14.m

--- Signals_and_Systems/g14.m
+++ Signals_and_Systems/g14.m
@@ -1,9 +1,9 @@
-t=-0.1:.001:0.1;%The time interval of the requested signal and its step .001 sec)
+t=-0.1:.001:0.1;                                                    %The time interval of the requested signal and its step .001 sec)
 z=cos(100*pi*t)+ cos(200*pi*t)+sin(500*pi*t);
 
 Ts1=0.002;%We set Ts1=0.002 sec
 N1=-50:1:50;
-Xs1=cos(100*pi*N1*Ts1)+cos(200*pi*N1*Ts1)+sin(500*pi*N1*Ts1);%The new signal with the same sampling frequency that we found in G.1.1
+Xs1=cos(100*pi*N1*Ts1)+cos(200*pi*N1*Ts1)+sin(500*pi*N1*Ts1);    %The new signal with the same sampling frequency that we found in G.1.1
 Ts2=0.0001;%We set a higher frequency to get a shorter period:f=10000 Hz
 N2=-1000:1:1000;
 Xs2=cos(100*pi*N2*Ts2)+cos(200*pi*N2*Ts2)+sin(500*pi*N2*Ts2);
@@ -19,12 +19,11 @@ end;
 figure('Name','Urgent Signal for Ts=0.0001');
 hold on;
 
-%Display of signals:
-plot(t,z);%Display of the signal G.1.2
+plot(t,z);                                                      %Display of the signal G.1.2
 hold on;
-plot(t,x1,'-r');%Display of the signal G.1.3 
+plot(t,x1,'-r');                                                %Display of the signal G.1.3 
 hold on;
-plot(t,x2,'-g');%Display of the signal G.1.4
+plot(t,x2,'-g');                                                %Display of the signal G.1.4
 legend('Xs(t)');
 legend('y(t)');
 TITLE ('G.1.4');
